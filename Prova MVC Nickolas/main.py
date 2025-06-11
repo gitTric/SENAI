cd87a52d751819eca1de269e7fab90b41b61b00e
gitTric <tricotisenai@outlook.com>
2025-06-11 15:43:29 -0300

    Delete Prova MVC Nickolas directory

--- Prova MVC Nickolas/main.py
+++
@@ -1,5 +0,0 @@
-from view.interface import Interface
-
-if __name__ == "__main__":
-    app = Interface()
-    app.menu()
--- Prova MVC Nickolas/model/produto.py
+++
@@ -1,5 +0,0 @@
-class Produto:
-    def __init__(self, nome, quantidade, preco):
-        self.nome = nome
-        self.quantidade = quantidade
-        self.preco = preco
